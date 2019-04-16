70184243f9186d39a7c91275299893cc851d1381
96weiBin <96weibin@gmail.com>
2019-04-16 08:04:40 +0800

    html与css基本完成

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,10 +1,17 @@
-$('.form_datetime').datetimepicker({
-  language:  'fr',
-  weekStart: 1,
-  todayBtn:  1,
-  autoclose: 1,
-  todayHighlight: 1,
-  startView: 2,
-  forceParse: 0,
-  showMeridian: 1
+$('.form_date').datetimepicker({
+	language: 'fr',
+	weekStart: 1,
+	todayBtn: 1,
+	autoclose: 1,
+	todayHighlight: 1,
+	startView: 2,
+	minView: 2,
+	forceParse: 0
 });
+// //模态框开关
+// $(".addNew").click(()=>{
+//   $(".insertMo")[0].style.display = 'block';
+// })
+// $(".closeBtn").click(()=>{
+// 	$('.insertMo').css({'display':'none'})
+// })
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,18 @@
 "use strict";
 
 $('.form_date').datetimepicker({
-    language:  'fr',
-    weekStart: 1,
-    todayBtn:  1,
-		autoclose: 1,
-		todayHighlight: 1,
-		startView: 2,
-		minView: 2,
-		forceParse: 0
-});
+  language: 'fr',
+  weekStart: 1,
+  todayBtn: 1,
+  autoclose: 1,
+  todayHighlight: 1,
+  startView: 2,
+  minView: 2,
+  forceParse: 0
+}); // //模态框开关
+// $(".addNew").click(()=>{
+//   $(".insertMo")[0].style.display = 'block';
+// })
+// $(".closeBtn").click(()=>{
+// 	$('.insertMo').css({'display':'none'})
+// })
